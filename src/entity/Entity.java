4b439a1e46084435145b48b857c7d351d8af34da
steveplak <67207728+steveplak@users.noreply.github.com>
2022-05-31 21:19:13 +0100

    not working commmit
    will fix hopefully in a bit

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -4,7 +4,7 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
-	public int x, y;
+	public int worldx, worldy;
 	public int speed;
 
 	public BufferedImage Bup, Bup1, Bup2, Bdown, Bdown1, Bright, Bright1, Bright2, Bright3, Bleft, Bleft1, Bleft2,
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,36 +10,34 @@ import javax.imageio.ImageIO;
 import main.GamePanel;
 import main.KeyHandler;
 
-public class Player extends Entity{
+public class Player extends Entity {
 	GamePanel gp;
 	KeyHandler keyH;;
-	
-	
-	public Player(GamePanel gp, KeyHandler keyH ){
-		
+
+	public Player(GamePanel gp, KeyHandler keyH) {
+
 		this.gp = gp;
 		this.keyH = keyH;
-		
+
 		solidArea = new Rectangle();
-		solidArea.x = 8;
-		solidArea.y = 8;
-		solidArea.width = 32;
-		solidArea.height = 32;
-		
-		
+		solidArea.x = 0;
+		solidArea.y = 0;
+		solidArea.width = 16;
+		solidArea.height = 16;
+
 		setDefaultValues();
 		getPlayerImage();
 	}
+
 	public void setDefaultValues() {
-		
-		x = 100;
-		y = 100;
-		speed = 4; 
-		direction = "down";
+
+		speed = 4;
+		direction = "right";
 	}
+
 	public void getPlayerImage() {
 		try {
-			
+
 			Bup = ImageIO.read(getClass().getResourceAsStream("/player/sprights/Blue/BUp/pixil-frame-0.png"));
 			Bup1 = ImageIO.read(getClass().getResourceAsStream("/player/sprights/Blue/BUp/pixil-frame-0 (1).png"));
 			Bup2 = ImageIO.read(getClass().getResourceAsStream("/player/sprights/Blue/BUp/pixil-frame-0 (2).png"));
@@ -53,118 +51,130 @@ public class Player extends Entity{
 			Bleft1 = ImageIO.read(getClass().getResourceAsStream("/player/sprights/Blue/BLeft/pixil-frame-0 (1).png"));
 			Bleft2 = ImageIO.read(getClass().getResourceAsStream("/player/sprights/Blue/BLeft/pixil-frame-0 (2).png"));
 			Bleft3 = ImageIO.read(getClass().getResourceAsStream("/player/sprights/Blue/BLeft/pixil-frame-0 (3).png"));
-			
-		}catch(IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void update() {
 		if ((keyH.upPressed) == true) {
-		direction = "up";
-			y -= speed;
-
+			direction = "up";
+			worldy -= speed;
 		} else if ((keyH.downPressed) == true) {
 			direction = "down";
-			y += speed;
-			
+			worldy += speed;
 		} else if ((keyH.leftPressed) == true) {
 			direction = "left";
-			x -= speed;
-			
+			worldx -= speed;
 		} else if ((keyH.rightPressed) == true) {
 			direction = "right";
-			x += speed;
-		
+			worldx += speed;
 		}
-		
+
 		collisionOn = false;
 		gp.cChecker.checkTile(this);
-		
+
+		if (collisionOn == false) {
+			switch (direction) {
+			case "up":
+				worldy -= speed;
+				break;
+			case "down":
+				worldy += speed;
+				break;
+			case "left":
+				worldx -= speed;
+				break;
+			case "right":
+				worldx += speed;
+				break;
+			}
+		}
 		spriteCounter++;
-		if(spriteCounter > 20) {
-			if(spriteNum == 1) {
+		if (spriteCounter > 20) {
+			if (spriteNum == 1) {
 				spriteNum = 2;
-			}else if(spriteNum == 2) {
+			} else if (spriteNum == 2) {
 				spriteNum = 3;
-			}else if(spriteNum == 3) {
+			} else if (spriteNum == 3) {
 				spriteNum = 4;
-			}else if(spriteNum == 4) {
+			} else if (spriteNum == 4) {
 				spriteNum = 5;
-			}else if(spriteNum == 5) {
+			} else if (spriteNum == 5) {
 				spriteNum = 6;
-			}else if(spriteNum == 6) {
+			} else if (spriteNum == 6) {
 				spriteNum = 7;
-			}else if(spriteNum == 7) {
+			} else if (spriteNum == 7) {
 				spriteNum = 8;
-			}else if(spriteNum == 8) {
+			} else if (spriteNum == 8) {
 				spriteNum = 9;
-			}else if(spriteNum == 9) {
+			} else if (spriteNum == 9) {
 				spriteNum = 10;
-			}else if(spriteNum == 10) {
+			} else if (spriteNum == 10) {
 				spriteNum = 1;
 			}
 			spriteCounter = 0;
 		}
 	}
+
 	public void draw(Graphics2D g2) {
-	//	g2.setColor(Color.white);
-	//	g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-		
-		
+		// g2.setColor(Color.white);
+		// g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
 		BufferedImage image = null;
-		
-		switch(direction) {
+
+		switch (direction) {
 		case "up":
-			if(spriteNum <= 8) {
+			if (spriteNum <= 8) {
 				image = Bup;
 			}
-			if(spriteNum == 9) {
+			if (spriteNum == 9) {
 				image = Bup1;
 			}
-			if(spriteNum == 10) {
+			if (spriteNum == 10) {
 				image = Bup2;
 			}
-			
+
 			break;
 		case "down":
-			if(spriteNum <= 9) {
+			if (spriteNum <= 9) {
 				image = Bdown;
 			}
-			if(spriteNum == 10) {
+			if (spriteNum == 10) {
 				image = Bdown1;
 			}
-		    break;
+			break;
 		case "left":
-			if(spriteNum <= 7) {
+			if (spriteNum <= 7) {
 				image = Bleft;
 			}
-			if(spriteNum == 8) {
+			if (spriteNum == 8) {
 				image = Bleft1;
 			}
-			if(spriteNum == 9) {
+			if (spriteNum == 9) {
 				image = Bleft2;
 			}
-			if(spriteNum == 10) {
+			if (spriteNum == 10) {
 				image = Bleft3;
 			}
 			break;
 		case "right":
-			if(spriteNum <= 7) {
+			if (spriteNum <= 7) {
 				image = Bright;
 			}
-			if(spriteNum == 8) {
+			if (spriteNum == 8) {
 				image = Bright1;
 			}
-			if(spriteNum == 9) {
+			if (spriteNum == 9) {
 				image = Bright2;
 			}
-			if(spriteNum == 10) {
+			if (spriteNum == 10) {
 				image = Bright3;
 			}
 			break;
 		}
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
-		
+		g2.drawImage(image, 100, 100, gp.tileSize, gp.tileSize, null);
+
 	}
 }
